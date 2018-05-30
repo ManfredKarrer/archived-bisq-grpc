44d61f392c827661ee2bfd5cb8af5eb58ed36bf7
Manfred Karrer <mk@nucleo.io>
2018-05-30 22:41:07 +0200

    Initial commit

---
+++ src/main/java/bisq/grpc/BisqGrpcClient.java
@@ -0,0 +1,114 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.grpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BisqGrpcClient {
+    private static final Logger logger = Logger.getLogger(BisqGrpcClient.class.getName());
+
+    private final ManagedChannel channel;
+    private final GetVersionGrpc.GetVersionBlockingStub getVersionStub;
+    private final GetBalanceGrpc.GetBalanceBlockingStub getBalanceStub;
+    private final StopServerGrpc.StopServerBlockingStub stopServerStub;
+
+    public static void main(String[] args) throws Exception {
+        BisqGrpcClient client = new BisqGrpcClient("localhost", 50051);
+        try (Scanner scanner = new Scanner(System.in);) {
+            while (true) {
+                String input = scanner.nextLine();
+                String result = "";
+                long startTs = System.currentTimeMillis();
+                if (input.equals("getVersion")) {
+                    result = client.getVersion();
+                } else if (input.equals("getBalance")) {
+                    result = String.valueOf(client.getBalance());
+                } else if (input.equals("stopClient")) {
+                    result = "Shut down client";
+                    client.shutdown();
+                } else if (input.equals("stopServer")) {
+                    client.stopServer();
+                    result = "Server stopped";
+                }
+
+                // First response is rather slow (300 ms) but following responses are fast (3-5 ms).
+                logger.log(Level.INFO, "Request took: {0} ms", System.currentTimeMillis() - startTs);
+                System.out.println(result);
+            }
+        }
+    }
+
+
+    private BisqGrpcClient(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port)
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+                // needing certificates.
+                .usePlaintext(true)
+                .build());
+    }
+
+    /**
+     * Construct client for accessing server using the existing channel.
+     */
+    private BisqGrpcClient(ManagedChannel channel) {
+        this.channel = channel;
+        getVersionStub = GetVersionGrpc.newBlockingStub(channel);
+        getBalanceStub = GetBalanceGrpc.newBlockingStub(channel);
+        stopServerStub = StopServerGrpc.newBlockingStub(channel);
+    }
+
+    private String getVersion() {
+        GetVersionRequest request = GetVersionRequest.newBuilder().build();
+        try {
+            return getVersionStub.getVersion(request).getVersion();
+        } catch (StatusRuntimeException e) {
+            return "RPC failed: " + e.getStatus();
+        }
+    }
+
+    private long getBalance() {
+        GetBalanceRequest request = GetBalanceRequest.newBuilder().build();
+        try {
+            return getBalanceStub.getBalance(request).getBalance();
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return -1;
+        }
+    }
+
+    private void stopServer() {
+        StopServerRequest request = StopServerRequest.newBuilder().build();
+        try {
+            stopServerStub.stopServer(request);
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+        }
+    }
+
+    private void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(1, TimeUnit.SECONDS);
+        System.exit(0);
+    }
+}
---
+++ src/main/java/bisq/grpc/BisqGrpcServer.java
@@ -0,0 +1,106 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.grpc;
+
+import bisq.core.Daemon;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class BisqGrpcServer {
+    private static final Logger logger = Logger.getLogger(BisqGrpcServer.class.getName());
+
+    private static Server server;
+    private static Daemon bisqDaemon;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final BisqGrpcServer server = new BisqGrpcServer(new Daemon());
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    private BisqGrpcServer(Daemon bisqDaemon) {
+        this.bisqDaemon = bisqDaemon;
+    }
+
+    private void start() throws IOException {
+        /* The port on which the server should run */
+        int port = 50051;
+        server = ServerBuilder.forPort(port)
+                .addService(new GetVersionImpl())
+                .addService(new GetBalanceImpl())
+                .addService(new StopServerImpl())
+                .build()
+                .start();
+        logger.info("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+            System.err.println("*** shutting down gRPC server since JVM is shutting down");
+            BisqGrpcServer.this.stop();
+            System.err.println("*** server shut down");
+        }));
+    }
+
+    private static void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    // Services
+    static class GetVersionImpl extends GetVersionGrpc.GetVersionImplBase {
+        @Override
+        public void getVersion(GetVersionRequest req, StreamObserver<GetVersionReply> responseObserver) {
+            GetVersionReply reply = GetVersionReply.newBuilder().setVersion(bisqDaemon.getVersion()).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+    }
+
+    static class GetBalanceImpl extends GetBalanceGrpc.GetBalanceImplBase {
+        @Override
+        public void getBalance(GetBalanceRequest req, StreamObserver<GetBalanceReply> responseObserver) {
+            GetBalanceReply reply = GetBalanceReply.newBuilder().setBalance(bisqDaemon.getBalance()).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+    }
+
+    static class StopServerImpl extends StopServerGrpc.StopServerImplBase {
+        @Override
+        public void stopServer(StopServerRequest req, StreamObserver<StopServerReply> responseObserver) {
+            StopServerReply reply = StopServerReply.newBuilder().build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+
+            stop();
+        }
+    }
+}
